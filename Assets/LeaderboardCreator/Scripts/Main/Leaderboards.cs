e2ecc74b5d02b55d91c522546483a4d9faf523ad
RIP-Skillan <ripskillan312004@gmail.com>
2025-10-19 16:44:50 +0530

    Unity Project Setup

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference Top_Fraggers = new LeaderboardReference("2a0084d0793666efe269cd297c0e5a912a9db8e3fa4de305d769f8f1d80ab0dc");
+    }
+}
