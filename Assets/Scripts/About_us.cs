eff756d5c1333ef11be2d2ed3e31f2ad575fc5fa
Harishankar.1 <88716735+Harishankar14@users.noreply.github.com>
2025-10-20 19:53:28 +0530

    First commit into Respryte

---
+++ Assets/Scripts/About_us.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class About_us: MonoBehaviour
+{
+    public void go_about()
+    {
+        SceneManager.LoadScene("About");
+    }
+}
